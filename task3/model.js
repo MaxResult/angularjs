93d8b98f0b21d58629e8bd15e6a2700409cd8780
Max <maximvashchuk@gmail.com>
2016-11-14 17:38:26 +0200

    task3. Begin

---
+++ task3/model.js
@@ -0,0 +1,10 @@
+var model = {
+    category: "Програмування",
+    books: [
+        { name: "Язык программирования C# 6.0 и платформа .NET 4.6, 7-е издание", author: "Эндрю Троелсен", electronicVersion : true },
+        { name: "Простой Python. Современный стиль программирования", author: "Билл Любанович", electronicVersion : true },
+        { name: "Изучаем Ruby. Серия Head First", author: "Джей Макгаврен", electronicVersion : true  },
+        { name: "Веб-разработка с применением Node и Express. Полноценное использование стека JavaScript", author: "Итан Браун", electronicVersion : false  },
+        { name: "Pro AngularJS", author: "A. Freeman", electronicVersion : false  }
+    ]
+};
