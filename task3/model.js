abc990fd8b084cabf9b62e31f8e109453a227e1f
Max <maximvashchuk@gmail.com>
2016-11-28 16:57:32 +0200

    task 3.

--- task3/model.js
+++ task3/model.js
@@ -1,10 +1,10 @@
 var model = {
     category: "Програмування",
     books: [
-        { name: "Язык программирования C# 6.0 и платформа .NET 4.6, 7-е издание", author: "Эндрю Троелсен", electronicVersion : true },
-        { name: "Простой Python. Современный стиль программирования", author: "Билл Любанович", electronicVersion : true },
-        { name: "Изучаем Ruby. Серия Head First", author: "Джей Макгаврен", electronicVersion : true  },
-        { name: "Веб-разработка с применением Node и Express. Полноценное использование стека JavaScript", author: "Итан Браун", electronicVersion : false  },
-        { name: "Pro AngularJS", author: "A. Freeman", electronicVersion : false  }
+        {name: "Язык программирования C# 6.0 и платформа .NET 4.6, 7-е издание", author: "Эндрю Троелсен", electronicVersion : true, description : "Нове 7-е видання цієї книги було повністю переглянуто і переписано з урахуванням останніх змін специфікації мови C # і нових досягнень платформи .NET Framework. Окремі глави присвячені важливим новим засобам, які роблять .NET Framework 4.6 самим передовим випуском" },
+        {name: "Простой Python. Современный стиль программирования", author: "Билл Любанович", electronicVersion : true, description : "Ця книга ідеально підходить як для початківців програмістів, так і для тих, хто тільки збирається освоювати Python, але вже має досвід програмування на інших мовах. У ній докладно розглядаються найсучасніші пакети і бібліотеки Python." },
+        {name: "Изучаем Ruby. Серия Head First", author: "Джей Макгаврен", electronicVersion : true,description : "Як і всі книги серії Head First, книга «Вивчаємо Ruby» використовує активний підхід до навчання, виходячи за рамки сухих, абстрактних пояснень і довідників. Вас не тільки навчать мови Ruby, але і допоможуть вашій програмістської зірці яскраво засяяти на небосхилі. Ви освоїте основи мови і просунуті можливості Ruby, такі як блоки, об'єкти, методи, класи і регулярні вирази. З поліпшенням ваших навичок завдання будуть ускладнюватися, і ви перейдете до таких тем, як обробка виключень, модулі, підмісив класи і метапрограмування."  },
+        {name: "Веб-разработка с применением Node и Express. Полноценное использование стека JavaScript", author: "Итан Браун", electronicVersion : false, description : "Ця книга призначена для програмістів, які бажають створювати веб-додатки (звичайні сайти, що втілюють REST-інтерфейси програмування додатків або щось середнє між ними) з використанням JavaScript, Node і Express. Для читання книги вам не буде потрібно досвід роботи з Node, проте необхідний хоча б невеликий досвід роботи з JavaScript."  },
+        {name: "Pro AngularJS", author: "A. Freeman", electronicVersion : false, description : "Adam Freeman is an experienced IT professional who has held senior positions in a range of companies, most recently serving as chief technology officer and chief operating officer of a global bank. Now retired, he spends his time writing and running."  }
     ]
 };
